d3dd4b43a7ad34c0e601e5e75f87e8e42b26240e
bkashigar <bkashigar@basavaraj.blr.riftio.com>
2019-09-20 16:39:19 +0530

    second commit

--- lxmon.py
+++ lxmon.py
@@ -1,26 +1,28 @@
+import sys
 import os
-
+from sys import exit
 def trigger(cmd):
+	print("================================================")
 	os.system(cmd)
+	print("================================================")
 	return
 
 print("================================================")
 print("lxmon started ..................................")
 print("================================================")
-print("1 - List of files in directory\n2- Detils of Processes/CPU Usage/Memory/Disks etc...")
+print(" 1 - Detils of Processes/CPU Usage/Memory/Disks etc...")
+print(" 2 - List network statistics")
+print(" 3 - Security information from linux processes")
 option=int(input())
 print(option)
 if option==1:
-	print("================================================")
-	print("List of files in directory")
-	print("================================================")
-	trigger('ls -ltraR')
-	print("================================================")
-	print("Current working directory")
-	trigger('pwd')
-	print("================================================")
-elif option==2:
-	print("================================================")
 	print("Processes/CPU Usage/Memory/Disks details")
 	trigger('top')
-	print("================================================")
+elif option==2:
+	print("List network statistics")
+	trigger('netstat')
+elif option==3:
+	print("Security information from linux processes")
+	trigger('ps -eM')
+else:
+	print("Enter proper option")
---
+++ readme.txt
@@ -0,0 +1,9 @@
+LXMON is a tool to monitor Linux/Mac machines.
+
+Requirements to run:
+Python3 installation
+
+How to run:
+Python3 lxmon.py
+
+
